340ff3a4c9934337200ef3f1cbdc7bd335a1b183
R-FUETA <raul.a.fuenzalida@gmail.com>
2025-06-23 11:52:16 -0400

    1.0.5

--- src/main/java/com/edutechms1/edutechms1/controller/UsuarioController.java
+++ src/main/java/com/edutechms1/edutechms1/controller/UsuarioController.java
@@ -20,33 +20,32 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
-
+    @Operation(summary = "Crear Usuario", description = "Permite Crear Usuarios")
     @PostMapping
     public Usuario crear(@RequestBody Usuario usuario) {
         return usuarioService.crearUsuario(usuario);
     }
-
+    @Operation(summary = "Obtener", description = "Obtiene Usuarios por ID")
     @GetMapping("id/{id}")
     public Usuario obtener(@PathVariable Long id) {
         return usuarioService.obtenerPorId(id);
     }
-
+    @Operation(summary = "Actualizar", description = "Actualizar Usuarios")
     @PutMapping("id/{id}")
     public Usuario actualizar(@PathVariable Long id, @RequestBody Usuario usuario) {
         return usuarioService.actualizarUsuario(id, usuario);
     }
-
+    @Operation(summary = "Eliminar", description = "Elimina Usuarios")
     @DeleteMapping("id/{id}")
     public void eliminar(@PathVariable Long id) {
         usuarioService.eliminarUsuario(id);
     }
-
+    @Operation(summary = "Obtener Usuarios", description = "Obtiene una lista con todos los usuarios")
     @GetMapping
-    @Operation(summary = "Obtener todos los usuarios", description = "Obtiene una lista con todos los usuarios")
     public List<Usuario> listar() {
         return usuarioService.listarUsuarios();
     }
-
+    @Operation(summary = "Login", description = "Iniciar session con tu correo y contraseña")
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestParam String correo, @RequestParam String contrasena) {
         Usuario usuario = usuarioService.autenticar(correo, contrasena);
